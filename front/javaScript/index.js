190e830af176b5b24692929192c21d4ab84675ab
AliNamooya <ali.namooya1@gmail.com>
2021-10-08 00:25:49 +0200

    Page d'accueil OK
    La fonction javaScript fonctionne, il y a que la mise en page a faire et le lien vers la page produit.

---
+++ front/javaScript/index.js
@@ -0,0 +1,27 @@
+fetch('http://localhost:3000/api/products')
+  .then(response => response.json())
+  .then(json => extractName(json)); //Une fois les données recues, on éxecute la fonction extractName//
+
+
+function extractName(data){
+
+  let items = document.getElementById('items');//on slectionne l'ID de la div du fichier HTML //
+
+
+  //Boucle for, si I= 0 et inférieur à 8 du coup on continue la boucle //
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+
+    let element = document.createElement('div');  //crée un div//
+
+    //---------contenue de la div---------//
+    element.innerHTML = `
+    <a href="#" class= productLink>
+    <img width= "350" height="350" src="${data[i]['imageUrl']}" alt="${data[i]['altTxt']}">
+    <h3 class="productName">${data[i]['name']}</h3>
+    <h2 class="productPrice">${data[i]['price']} €</h2>
+    </a>
+    `  
+    items.appendChild(element) //insérer l'élement dans le HTML//
+  }
+}
